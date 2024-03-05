25130385e7a5ee5666d60c5f54f3bff9fba27fb2
Stefan Spataro <stefanspataro@hotmail.com>
2024-03-05 13:18:45 -0800

    added user class methods (no functionality yet)

--- AvengersAssemble/tixx/models.py
+++ AvengersAssemble/tixx/models.py
@@ -8,3 +8,37 @@ class Event(models.Model):
 
     def __str__(self):
         return self.title
+
+class User():
+    from django import connection
+    userid = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+
+    def getReviews():
+        with connection.cursor() as curson:
+            cursor.execute(sql)
+            rows = cursor.fetchall()
+
+        #sql to return:
+        #SELECT * FROM reviews WHERE userid=(this.userid)
+
+    def getPayments():
+    
+    def getTickets():
+    
+    def __str__(self):
+        return self.username + ", " + self.email + ", " + self.phone + ", " + self.address + ", " + userid
+
+    def buyTickets():
+
+    def addToCart():
+
+    def deleteFromCart():
+        
+    def makePayment():
+
+    
+    
