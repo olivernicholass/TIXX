fa76a0abbb9e5d9c24f8a4905106743ce1f35322
olivernicholass <omedgyesi@icloud.com>
2024-03-06 18:29:49 -0800

    Created Multiple Empty Views + Tests

--- AvengersAssemble/tixx/urls.py
+++ AvengersAssemble/tixx/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
-    path('events/', views.events, name='events'),
+    path('', views.home, name='home'),
+    path('login/', views.login, name='login'),
+    path('profile/', views.profile, name='profile'),
+    path('register/', views.register, name='register'),
+    path('search/', views.search_results, name='search_results'),
+    path('tickets/', views.ticket_selection, name='ticket_selection'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('filteredevents/', views.filtered_events, name='filtered_events'),
 ]
-
