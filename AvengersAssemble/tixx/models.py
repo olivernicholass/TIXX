56883028fc7b7f8f3810a72b559c4d6de45e627f
prateek-balani <prateekbalani03@gmail.com>
2024-03-07 20:04:59 -0800

    initial fixes

--- AvengersAssemble/tixx/models.py
+++ AvengersAssemble/tixx/models.py
@@ -1,62 +1,63 @@
+
 from django.db import models
 
 # Create your models here.
 
 
 class Event(models.Model):
-    eventName = models.CharField(max_length=100)
+    eventName = models.CharField(max_length=100, default="No Name provided")
     eventDate = models.DateField()
-    eventId = models.IntegerField(max_length=10 ,primary_key=True)
+    eventId = models.CharField(primary_key=True, max_length=5)
     eventLocation = models.CharField(max_length=100)
-    eventDescription = models.CharField(max_length=250)
-    eventStatus = models.CharField(max_length=10)
+    eventDescription = models.CharField(max_length=250, default="No description provided")
+    eventStatus = models.CharField(max_length=10, default="No status provided")
 
 
     def __str__(self):
-        return self.title
+        return self.eventId
 
 class Ticket(models.Model):
-    ticketId = models.IntegerField(max_length=10, primary_key=True)
-    eventId = models.ForeignKey(Event, on_delete=models.SET_NULL)
+    ticketId = models.IntegerField(primary_key=True)
+    eventId = models.ForeignKey(Event, on_delete=models.SET_NULL, null=True)
     seatNum = models.CharField(max_length=5)
     ticketQR = models.CharField(max_length=250)
     ticketPrice = models.IntegerField()
     ticketType = models.CharField(max_length=10)
 
     def __str__(self):
-        return self.title
+        return str(self.ticketId)
 
 
 class User(models.Model):
     userId = models.CharField(max_length=10, primary_key=True)
     username = models.CharField(max_length=20)
     userEmail = models.EmailField()
-    userPhoneNumber = models.IntegerField(max_length=10)
+    userPhoneNumber = models.IntegerField()
     userAddress = models.CharField(max_length=100)
 
 
     def __str__(self):
-        return self.title
+        return self.userId
 
 class Payment(models.Model):
     paymentId = models.CharField(max_length=10, primary_key=True)
-    userId = models.ForeignKey(User, on_delete=models.SET_NULL)
+    userId = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     paymentAmount = models.FloatField()
     paymentMethod = models.CharField(max_length=10)
     paymentDate = models.DateField()
     transactionId = models.CharField(max_length=10)
 
     def __str__(self):
-        return self.title
+        return self.paymentId
 
 class Review(models.Model):
-    reviewId = models.AutoField() # autofield sets it to a primary key automatically 
+    reviewId = models.CharField(max_length=10, primary_key=True) # autofield sets it to a primary key automatically 
     reviewRating = models.IntegerField()
     reviewTitle = models.CharField(max_length=10)
     reviewText = models.CharField(max_length=50)
-    eventID = models.ForeignKey(User, on_delete=models.SET_NULL)
+    eventID = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     reviewDate = models.DateField()
 
     def __str__(self):
-        return self.title
+        return self.reviewId
 
