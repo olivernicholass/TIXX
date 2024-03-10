8ec2ee098687294b4e2a2438ad8fe97441de8f68
olivernicholass <omedgyesi@icloud.com>
2024-03-09 19:55:09 -0800

    Created Navbar for all pages + Navbar Functionality for filtering by genre

--- AvengersAssemble/tixx/admin.py
+++ AvengersAssemble/tixx/admin.py
@@ -1,5 +1,9 @@
+# admin.py
 from django.contrib import admin
+from .models import Event, Ticket, User, Payment, Review
 
-# Register your models here.
-
-
+admin.site.register(Event)
+admin.site.register(Ticket)
+admin.site.register(User)
+admin.site.register(Payment)
+admin.site.register(Review)
--- AvengersAssemble/tixx/views.py
+++ AvengersAssemble/tixx/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, HttpResponse
-
-
+from django.shortcuts import render
+from .models import Event
+from django.shortcuts import redirect
 # Create your views here.
 
 def home(request):
@@ -24,8 +24,9 @@ def ticket_selection(request):
 def checkout(request):
     return render(request, "checkout.html")
 
-def filtered_events(request):
-    return render(request, "filtered_events.html")
+def filtered_events(request, eventGenre):
+    filtered_events = Event.objects.filter(eventGenre=eventGenre)
+    return render(request, 'filtered_events.html', {'filtered_events': filtered_events})
 
 def figure(request):
     return render(request, "figure.html")
