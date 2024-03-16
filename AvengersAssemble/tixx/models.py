38f2720053d3e81f3ed7b341d2c059ba40f130cd
prateek-balani <prateekbalani03@gmail.com>
2024-03-16 14:01:25 -0700

    final final change

--- AvengersAssemble/tixx/models.py
+++ AvengersAssemble/tixx/models.py
@@ -18,7 +18,7 @@ class Event(models.Model):
     eventLocation = models.CharField(max_length=100)
     eventDescription = models.CharField(max_length=250)
     eventStatus = models.CharField(max_length=10)
-    eventGenre = models.CharField(max_length=100)  
+    eventGenre = models.CharField(max_length=100, default="none")  
     eventImage = models.ImageField(upload_to='event_images/', null=True, blank=True)  
     arenaId = models.ForeignKey(Arena,on_delete=models.SET_NULL, null=True)
 
