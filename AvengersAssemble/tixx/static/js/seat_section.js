25c9124a530b655874f4b02dcf77f1460c345a80
volumeOG <stefanspataro@hotmail.com>
2024-03-17 22:29:44 -0700

    added field, buttons, and seats with js

--- AvengersAssemble/tixx/static/js/seat_section.js
+++ AvengersAssemble/tixx/static/js/seat_section.js
@@ -1,9 +1,26 @@
-document.getElementById("section1").addEventListener("click", function() {
-    var table = document.getElementById("section1table");
-    console.log("1");
-    if (table.style.display === "none") {
-        table.style.display = "block";
-    } else {
-        table.style.display = "none";
+const showGridButton = document.getElementById('section1');
+const gridContainer = document.getElementById('gridContainer');
+
+function showSection1AvailableSeats(){
+  if (gridContainer.classList.contains('hidden')) {
+    gridContainer.classList.remove('hidden');
+    createGrid();
+    alert("grid created");
+  } else {
+    gridContainer.classList.add('hidden');
+    gridContainer.innerHTML = ''; // Clear the grid
+  }
+};
+
+function createGrid() {
+  for (let i = 0; i < 5; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    gridContainer.appendChild(row);
+    for (let j = 0; j < 10; j++) {
+      const seat = document.createElement('div');
+      seat.classList.add('seat');
+      gridContainer.appendChild(seat);
     }
-});
+  }
+}
