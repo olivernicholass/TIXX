11f75628aecb06c4c69104474dc49c9e3a2efbd0
Rory <roryhazlett42@hotmail.com>
2024-03-28 23:53:39 -0600

    forgot password functionality

--- AvengersAssemble/tixx/urls.py
+++ AvengersAssemble/tixx/urls.py
@@ -1,7 +1,8 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path
+from django.urls import path, include
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("", views.home, name="home"),
@@ -21,7 +22,10 @@ urlpatterns = [
     path('confirmation/', views.confirmation, name='confirmation'),
     path('organiser/login/', views.organiser_login, name='organiser_login'),
     path('organiser/register/', views.organiser_register, name='organiser_register'),
-
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name='reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
