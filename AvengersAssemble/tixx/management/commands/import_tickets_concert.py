4d6eb00dc3b2b53a1ea9e63896b69aa2804b52c6
volumeOG <stefanspataro@hotmail.com>
2024-04-09 21:44:20 -0700

    minor changes to ticket selection UI

--- AvengersAssemble/tixx/management/commands/import_tickets_concert.py
+++ AvengersAssemble/tixx/management/commands/import_tickets_concert.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from tixx.models import Ticket  # Import your Ticket model
+from tixx.models import Ticket
 import sys
 import random
 
@@ -19,16 +19,16 @@ class Command(BaseCommand):
         for s in range(1, 5):
             for j in range(1, 26):
                 letter = chr(j + ord('A') - 1)
-                for i in range(1, 26):  # Assuming you want to create 50 tickets
+                for i in range(1, 26): 
                     available = random.choice([True, False])
                     data.append({
-                        'eventId': event_id,  # Assuming eventId corresponds to the Event ID
-                        'seatNum': f'S{s}{letter}{i}',  # Generate seat numbers dynamically
-                        'arenaId': arena_id,  # Assuming arenaId corresponds to the Arena ID
-                        'ticketQR': 'x',  # Generate QR codes dynamically
-                        'ticketPrice': 100,  # Generate ticket prices dynamically
-                        'ticketType': 'normal',  # Assuming all tickets are Regular type
-                        'zone': s,  # Distribute zones from 1 to 8 cyclically
+                        'eventId': event_id, 
+                        'seatNum': f'S{s}{letter}{i}', 
+                        'arenaId': arena_id, 
+                        'ticketQR': 'x',
+                        'ticketPrice': 100,
+                        'ticketType': 'normal',
+                        'zone': s,
                         'available': available,
                     })
             
